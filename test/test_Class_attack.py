bd755d1f369d96b74f61e0db00c61335507914bc
AntoineVenier <antoine.venier@hotmail.fr>
2020-02-13 16:13:23 +0100

    test

--- test/test_Class_attack.py
+++ test/test_Class_attack.py
@@ -1,8 +1,7 @@
 from program.Class_attack import attack
 from program.Class_player import player
 
-def test_tir(attack):
-    player_test = player(6,5,7,9,5)
-    assert attack.tire(1,4,2,player_test,1)==1
+def test_tire():
+    at=attack()
+    assert at.tire(1,5,2,1)
 
-    
