b99206f8b4bdd1927add49a90b2483d161db2efd
AntoineVenier <antoine.venier@hotmail.fr>
2020-02-13 16:35:13 +0100

    Tests class_attack

--- test/test_Class_attack.py
+++ test/test_Class_attack.py
@@ -3,5 +3,16 @@ from program.Class_player import player
 
 def test_tire():
     at=attack()
-    assert at.tire(1,5,2,1)
+    assert at.tire(1,5,2,1)==1
+    assert at.tire(1,5,2,0)==0
+
+def test_tire_x_case():
+    at=attack()
+    assert at.tire_x_case(1,5,2,0,4)==0
+    at.tire_x_case(1,5,2,1,4)
+    assert at.tir == 1
+    
+    
+    
+
 
