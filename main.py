778da52d5d1320dd51978915b1e9cb4671f00078
ThomasAbg <thomas.dupont37@gmail.com>
2020-02-16 10:49:49 +0100

    make class client

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import time
 import datetime
 from multiprocessing import Queue
 
-from client import connectClient, ClientSend, exitClient
+from client import connectClient, clientsend, exitclient
 
 
 status = Queue(maxsize=0)
@@ -26,7 +26,7 @@ statusclient = Queue(maxsize=0)
 
 def running():
     """Run of the main thread."""
-    statusConnect = 0
+    statusconnect = 0
     while 1:
         # interface code client
         if not statusclient.empty():
@@ -41,31 +41,31 @@ def running():
 
         # interface code window
         elif not status.empty():
-            dataReceive = status.get()
-            print("Sys msg from Frame: ", dataReceive)
+            data_receive = status.get()
+            print("Sys msg from Frame: ", data_receive)
 
-            if dataReceive[0] == "Connect":
-                # dataReceive: IP, Port, Pseudo
+            if data_receive[0] == "Connect":
+                # data_receive: IP, Port, Pseudo
                 print("Start processus client")
                 statusclient.put(
-                    ["RunClient", dataReceive[1], dataReceive[2]],
-                    dataReceive[3],
+                    ["RunClient", data_receive[1], data_receive[2]],
+                    data_receive[3],
                     True,
                 )
-                statusConnect = 1
-            elif dataReceive[0] == "Pseudo":
-                ClientSend(11, dataReceive[1])
+                statusconnect = 1
+            elif data_receive[0] == "Pseudo":
+                clientsend(11, data_receive[1])
 
-            elif dataReceive[0] == "text" and statusConnect == 1:
-                ClientSend(7, dataReceive[1])
-            elif dataReceive[0] == "text" and statusConnect == 0:
+            elif data_receive[0] == "text" and statusconnect == 1:
+                clientsend(7, data_receive[1])
+            elif data_receive[0] == "text" and statusconnect == 0:
                 print(
                     "Try to send data but not connected to server at ",
                     datetime.datetime.now(),
                 )
 
-            if dataReceive == "exit":
-                exitClient()
+            if data_receive == "exit":
+                exitclient()
                 print("Main close")
                 break
         else:
