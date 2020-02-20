8504c18b8a9b259b6619156675301b2611cec5fa
Axel Jacquot <axel.jacquot@outlook.com>
2020-02-20 15:19:04 +0100

    Delete Tir

--- program/Class_defend.py
+++ program/Class_defend.py
@@ -2,21 +2,10 @@ from program.Class_player import player
 
 
 class defend:
-    def case_tire_1(self, x, y, layer, tir, player):
-        if tir == 1:
-            if player.map_allied[layer, y, x] != 0:
-                player.map_allied[layer, y, x] = 9
-                self.touch = 1
-            else:
-                self.touch = 0
-            return self.touch
-        if tir == 2:
-            if player.map_allied[layer, y, x] != 0:
-                for i in range(2):
-                    for j in range(2):
-                        if player.map_allied[layer, y + j, x + i] != 0:
-                            player.map_allied[layer, y + j, x + i] = 9
-                            self.touch = 1
-                        else:
-                            self.touch = 0
-            return self.touch
+    def case_tire_1(self, x, y, layer, player):
+        if player.map_allied[layer, y, x] != 0:
+            player.map_allied[layer, y, x] = 9
+            self.touch = 1
+        else:
+            self.touch = 0
+        return self.touch
