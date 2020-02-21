043a02678f52c88739ba9189dcb0b217583987f0
AntoineVenier <antoine.venier@hotmail.fr>
2020-02-21 12:18:37 +0100

    test

--- test/test_Class_player.py
+++ test/test_Class_player.py
@@ -1,1 +1,8 @@
-# essayer de mettre une valeur en dehors de 0 et 15 voir s'il y a une erreur
+# essayer de mettre une valeur en dehors de 0 et 15 voir s'il y a une 
+from program.Class_player import player
+
+def test_cleanBoat():
+    pl = player()
+    pl.map_allied[1, 5, 8] = 1
+    pl.cleanBoat(5,5,8,8,1,1)
+    assert pl.map_allied[1, 5, 8] == 0
