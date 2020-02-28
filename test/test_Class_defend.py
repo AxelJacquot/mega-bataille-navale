c83d9cb540be841399c74bb2f34ebfe3468ed5e7
AntoineVenier <antoine.venier@hotmail.fr>
2020-02-28 11:41:35 +0100

    test calss_player

--- test/test_Class_defend.py
+++ test/test_Class_defend.py
@@ -2,15 +2,13 @@ from program.LogicGame.Class_defend import defend
 from program.LogicGame.Class_player import player, map_allied
 
 
-#df = defend()
 
-
-#def test_case_tire():
-
-#    map_allied[2, 6, 5]= 5
-#    df.case_tire(5,6)
-#    assert touche == 1
-#    map_allied[2, 6, 5]= 0
-#    df.case_tire(5,6,2)
-#    assert touche == 0
+def test_case_tire():
+    df = defend()
+    map_allied[2, 6, 5]= 5
+    df.case_tire(5,6)
+    assert df.touch == 1
+    map_allied[2, 6, 5]= 0
+    df.case_tire(5,6)
+    assert df.touch == 0
     
